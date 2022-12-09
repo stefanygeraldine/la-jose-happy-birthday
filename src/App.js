957e5072f692fa1ae49eed56299987ffe8f55be7
Stefany Carballo <stefanygeraldine@gmail.com>
2022-12-09 19:33:21 -0300

    wip

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="container">
+     <div className="container__center">
+       <h1 className="animate-charcter"> Happy </h1>
+       <h2 className="animate-charcter"> Birthday</h2>
+       <h4>Josefina Losada</h4>
+       <p>AMIGA | TEACHER | COMPAÑERA</p>
+     </div>
+    </div>
+  );
+}
+
+export default App;
